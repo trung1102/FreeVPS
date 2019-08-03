29c88937d596a98004dc5e002c94cb2f67d27782
trung1102 <44827978+trung1102@users.noreply.github.com>
2019-08-03 10:04:04 +0700

    Update FreeVPSChannel.sh

--- FreeVPSChannel.sh
+++ FreeVPSChannel.sh
@@ -3,11 +3,6 @@
 #Vars
 mounted=0
 GREEN='\033[1;32m';GREEN_D='\033[0;32m';RED='\033[0;31m';YELLOW='\033[0;33m';BLUE='\033[0;34m';NC='\033[0m'
-# Virtualization checking..
-#virtu=$(egrep -i '^flags.*(vmx|svm)' /proc/cpuinfo | wc -l)
-if [ $virtu = 0 ] ; then echo -e "[Error] ${RED}Virtualization/KVM in your Server/VPS is OFF\nExiting...${NC}";
-else
-#
 # Deleting Previous Windows Installation by the Script
 #umount -l /mnt /media/script /media/sw
 #rm -rf /mediabots /floppy /virtio /media/* /tmp/*
